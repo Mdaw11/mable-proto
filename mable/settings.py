4bf298f1a68c858c789a96ac391093f53d1f8698
Miko <mikhaeldaw99@gmail.com>
2023-03-02 14:48:29 -0600

    whitenoise

--- mable/settings.py
+++ mable/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'projects',
     'tickets',
     'users',
@@ -51,6 +52,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,12 +131,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, "static"),
+# ]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
